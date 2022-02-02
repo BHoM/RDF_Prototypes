a1e6d1bff6de8d154c6811504f1f489038d097c2
DiellzaElshani <diellzaelshanni@gmail.com>
2022-02-02 16:50:22 +0100

    Update Tests.cs
    added description

--- RDF_ConsoleApp/Tests.cs
+++ RDF_ConsoleApp/Tests.cs
@@ -1,20 +1,11 @@
-﻿using BH.oM.Analytical.Results;
-using BH.oM.Base;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using BH.Engine.RDF;
-using VDS.RDF;
-using VDS.RDF.Writing;
+﻿using BH.Engine.RDF;
 using BH.oM.Architecture.Elements;
+using BH.oM.Base;
 using BH.oM.Physical.Elements;
 using BH.oM.RDF;
 using System.ComponentModel;
+using VDS.RDF;
+using VDS.RDF.Writing;
 
 namespace BH.oM.CodeAnalysis.ConsoleApp
 {
@@ -62,13 +53,14 @@ namespace BH.oM.CodeAnalysis.ConsoleApp
             g.WriteToXMLFile(_fileDirectory, "Triple_Test_XML.rdf");
         }
 
-        [Description("Code from Diellza. TODO: write description.")]
+        [Description("Code from Diellza: Tries to create owl:Classes (Structure_oM_Elements_Bar and Physical_oM_Elements_Beam) as subject and objects connectected by owl:ObjectProperties as predicate (hasElement or isPartOf). Uses owl and rdf vocabulary.")]
         public static void RoomWithBar()
         {
             IGraph g = new Graph();
 
             // Set the namespace of our Ontology
-            g.NamespaceMap.AddNamespace("bhom", UriFactory.Create("http://visualdataweb.org/newOntology/"));
+            // g.NamespaceMap.AddNamespace("bhom", UriFactory.Create("http://visualdataweb.org/newOntology/"));
+
             g.NamespaceMap.AddNamespace("owl", UriFactory.Create("http://www.w3.org/2002/07/owl"));
 
             var a = g.CreateUriNode("rdf:type");
@@ -77,28 +69,55 @@ namespace BH.oM.CodeAnalysis.ConsoleApp
             var rdfsLabel = g.CreateUriNode("rdfs:label");
             var rdfsDomain = g.CreateUriNode("rdfs:domain");
             var rdfsRange = g.CreateUriNode("rdfs:range");
-            var rdfsSubclassOf = g.CreateUriNode("rdfs:subclassOf");
-            var rdfsCharac = g.CreateUriNode("rdfs:charac");
+            var rdfsSubPropertyOf = g.CreateUriNode("rdfs:subPropertyOf");
 
-            IUriNode BHoM_Architecture_oM_Elements_Room = g.CreateUriNode(UriFactory.Create("https://github.com/BHoM/BHoM/blob/main/Architecture_oM/Elements/Room.cs"));
-            g.Assert(BHoM_Architecture_oM_Elements_Room, a, owlClass);
-            g.Assert(BHoM_Architecture_oM_Elements_Room, rdfsLabel, g.CreateLiteralNode("BHoM_Architecture_oM_Elements_Room", "en"));
+
+            var perimeter = g.CreateUriNode("rdfs:literal");
+            var rdfsLiteral = g.CreateUriNode("rdfs:literal");
+
+
+            //var rdfsSubclassOf = g.CreateUriNode("rdfs:subclassOf");
+
 
             IUriNode Structure_oM_Elements_Bar = g.CreateUriNode(UriFactory.Create("https://github.com/BHoM/BHoM/blob/main/Structure_oM/Elements/Bar.cs"));
             g.Assert(Structure_oM_Elements_Bar, a, owlClass);
             g.Assert(Structure_oM_Elements_Bar, rdfsLabel, g.CreateLiteralNode("Structure_oM_Elements_Bar", "en"));
 
+            g.Assert(Structure_oM_Elements_Bar, rdfsLiteral, perimeter);
+
+
+            IUriNode Physical_oM_Elements_Beam = g.CreateUriNode(UriFactory.Create("https://github.com/BHoM/BHoM/blob/main/Physical_oM/Elements/Beam.cs"));
+            g.Assert(Physical_oM_Elements_Beam, a, owlClass);
+            g.Assert(Physical_oM_Elements_Beam, rdfsLabel, g.CreateLiteralNode("Physical_oM_Elements_Beam", "en"));
+
+
+
             // Relation
-            IUriNode isPartOf = g.CreateUriNode("rdf:type");
+            IUriNode isPartOf = g.CreateUriNode(UriFactory.Create("https://github.com/BHoM/Predicate/isPartOf"));
             g.Assert(isPartOf, a, owlObjectProperty);
             g.Assert(isPartOf, rdfsLabel, g.CreateLiteralNode("isPartOf", "en"));
-            g.Assert(isPartOf, rdfsDomain, BHoM_Architecture_oM_Elements_Room);
-            g.Assert(isPartOf, rdfsRange, Structure_oM_Elements_Bar);
 
-            RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
-            rdfxmlwriter.Save(g, "BHoMGraph.rdf");
+           // g.Assert(isPartOf, rdfsDomain, Physical_oM_Elements_Beam);
+           // g.Assert(isPartOf, rdfsRange, Structure_oM_Elements_Bar);
 
-            g.WriteToXMLFile(_fileDirectory, "RoomWithBar.rdf");
+
+
+            // Relation
+            IUriNode hasElement = g.CreateUriNode(UriFactory.Create("https://github.com/BHoM/Predicate/hasElement"));
+            g.Assert(hasElement, a, owlObjectProperty);
+            g.Assert(hasElement, rdfsLabel, g.CreateLiteralNode("hasElement", "en"));
+            g.Assert(hasElement, rdfsSubPropertyOf, isPartOf);
+
+
+            g.Assert(hasElement, rdfsDomain, Structure_oM_Elements_Bar);
+            g.Assert(hasElement, rdfsRange, Structure_oM_Elements_Bar);
+     
+                            
+
+           g.WriteToXMLFile(_fileDirectory, "RoomWithBar.rdf");
+
+
+
         }
 
         [Description("Tries to create an ontology with a `Room` and `Column` class, to say that they both are subclass of `IObject`." +
