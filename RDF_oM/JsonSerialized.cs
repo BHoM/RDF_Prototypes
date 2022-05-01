ff8ccb68dbba5aeadb4a9a284f141eb1515e169a
Alessio Lombardi <alessio.lombardi@burohappold.com>
2022-05-01 15:12:45 +0100

    Create JsonSerialized.cs

---
+++ RDF_oM/JsonSerialized.cs
@@ -0,0 +1,15 @@
+﻿using BH.oM.Analytical.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.RDF
+{
+    public class JsonSerialized
+    {
+        public Type Type { get; set; }
+        public string Json { get; set; }
+    }
+}
