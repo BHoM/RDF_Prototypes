119396d8ce72bdfdf8fd7132bb1f0b0eeb4d420a
DiellzaElshani <diellzaelshanni@gmail.com>
2022-07-26 11:23:23 +0200

    Centralized test settings in parent class

---
+++ RDF_ConsoleApp/TTLTests/Test.cs
@@ -0,0 +1,21 @@
+﻿using BH.oM.RDF;
+
+namespace BH.Test.RDF
+{
+    /// <summary>
+    /// Base class for tests, with customised settings.
+    /// </summary>
+    public abstract class Test
+    {
+        public static LocalRepositorySettings m_localRepositorySettings = new LocalRepositorySettings()
+        {
+            GitRootPath = @"C:\Users\diels\source\repos"
+        };
+
+        public static OntologySettings m_ontologySettings = new OntologySettings()
+        {
+            ABoxSettings = new ABoxSettings() { IndividualsBaseAddress = "individuals.Address" },
+            TBoxSettings = new TBoxSettings() { CustomObjectTypesBaseAddress = "CustomObjectTypes.Address" }
+        };
+    }
+}
