ff92f7e77e3292817dbd89b8ea73327faaf62cca
Alessio Lombardi <alessio.lombardi@burohappold.com>
2022-07-26 12:28:28 +0100

    Update ToCustomObject.cs

--- RDF_Engine/Convert/ToCustomObject.cs
+++ RDF_Engine/Convert/ToCustomObject.cs
@@ -6,28 +6,34 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using BH.Engine.Reflection;
 
 namespace BH.Engine.RDF
 {
     public static partial class Convert
     {
         [Description("Attempts to get the properties of the object and use them to populate a BHoM CustomObject.")]
-        public static IObject ToCustomObject(this object obj)
+        public static object ToBHoM(this object obj)
         {
-            if (obj is IObject)
-                return (IObject)obj;
+            if (obj == null)
+                return null;
+
+            Type objType = obj.GetType();
+
+            if (obj is IObject || objType.IsPrimitive())
+                return obj;
 
             var customObject = new CustomObject();
 
             // For Speckle custom objects, we can get the hidden dynamic members by attempting an invokation of GetMembers.
-            var method = obj.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Where(m => m.Name.Contains("GetMembers")).FirstOrDefault();
+            var method = objType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Where(m => m.Name.Contains("GetMembers")).FirstOrDefault();
             if (method != null)
             {
                 // The result should be castable if the original object was a speckle custom object.
                 var valuesDict = method.Invoke(obj, null) as Dictionary<string, object>;
                 if (valuesDict != null)
                     foreach (var kv in valuesDict)
-                        customObject.CustomData[kv.Key] = kv.Value.ToCustomObject();
+                        customObject.CustomData[kv.Key] = kv.Value.ToBHoM();
 
                 return customObject;
             }
@@ -37,10 +43,10 @@ namespace BH.Engine.RDF
             bool includePrivateProperties = false;
             var bindingFlags = includePrivateProperties ? BindingFlags.NonPublic | BindingFlags.Public : BindingFlags.Public;
             // Try to convert based on any public property.
-            var publicProperties = obj.GetType().GetProperties(bindingFlags);
+            var publicProperties = objType.GetProperties(bindingFlags);
             if (publicProperties != null)
                 foreach (var prop in publicProperties)
-                    customObject.CustomData[prop.NameValidChars()] = prop.GetValue(obj).ToCustomObject();
+                    customObject.CustomData[prop.NameValidChars()] = prop.GetValue(obj).ToBHoM();
 
             return customObject;
         }
