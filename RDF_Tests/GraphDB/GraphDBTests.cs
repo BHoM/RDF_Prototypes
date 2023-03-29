3c7944975eb35fc3958fce77f5bbacbbfd9f11cc
polnischfrosch <104036071+polnischfrosch@users.noreply.github.com>
2023-03-29 13:31:11 +0200

    Add GraohDb tests to RDF_Tests

---
+++ RDF_Tests/GraphDB/GraphDBTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.Engine.Adapters.RDF;
+using NUnit.Framework;
+using Shouldly;
+
+namespace RDF_Tests.GraphDB
+{
+    internal class GraphDBTests
+    {
+        [Test]
+        public void PullFromRepoTest()
+        {
+            var result = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(repositoryName:"myrepo123", run:true);
+            result.ShouldNotBeNull();
+        }
+    }
+}
