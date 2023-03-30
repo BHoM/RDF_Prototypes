c59f7bccac1ee6025e0410ba2e47f88fab0e8bc8
Alessio Lombardi <alessio.lombardi@burohappold.com>
2023-03-30 16:30:53 +0100

    Update GraphDBTests.cs

--- RDF_Tests/GraphDB/GraphDBTests.cs
+++ RDF_Tests/GraphDB/GraphDBTests.cs
@@ -11,9 +11,12 @@ namespace RDF_Tests.GraphDB
 {
     internal class GraphDBTests
     {
-        [Test]
+        //[Test] // Restore when fixed as below.
         public void PullFromRepoTest()
         {
+            // This test should be corrected to first verify if the connectivity to GraphDB is available.
+            // If not available, the test should not run, to avoid false failures. Commenting out the code below.
+
             var result = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(repositoryName:"myrepo123", run:true);
             result.ShouldNotBeNull();
         }
