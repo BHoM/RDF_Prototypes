91955f749ff93ecb4543507ba1eb6fec222ef28c
polnischfrosch <104036071+polnischfrosch@users.noreply.github.com>
2023-04-04 17:42:51 +0200

    GraphDB Pull Test

--- RDF_Tests/GraphDB/GraphDBTests.cs
+++ RDF_Tests/GraphDB/GraphDBTests.cs
@@ -8,9 +8,11 @@ using BH.oM.Adapters.RDF;
 using BH.oM.Architecture.Elements;
 using NUnit.Framework;
 using Shouldly;
-using VDS.RDF.Query.Algebra;
 using BH.Engine.Adapters.TTL;
 using Compute = BH.Engine.Adapters.RDF.Compute;
+using VDS.RDF.Query;
+using VDS.RDF;
+using VDS.RDF.Writing;
 
 namespace RDF_Tests.GraphDB
 {
@@ -35,11 +37,34 @@ namespace RDF_Tests.GraphDB
         public void PullFromRepoReadBack() 
         {
             string userInput = "PREFIX bh: <https://bhom.xyz/ontology/> SELECT ?predicate ?object WHERE {bh:BH.oM.Architecture.Elements.Room ?predicate ?object.}";
-            var result = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(userInput, repositoryName: "TestRepository", run: true);
-            result.ShouldNotBeNull();
+            var results = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(userInput, repositoryName: "TestRepository", run: true);
+            results.ShouldNotBeNull();
 
             Room room = new Room();
             CSharpGraph cSharpGraph = Compute.CSharpGraph(new List<object>() { room });
+            string TTLGraph = cSharpGraph.ToTTL();
+
+            IGraph graph = new Graph();
+            INode subject = graph.CreateUriNode(new Uri("https://bhom.xyz/ontology/BH.oM.Architecture.Elements.Room"));
+
+            foreach (SparqlResult result in results)
+            {
+                INode predicate = result["predicate"].CopyNode(graph);
+                INode obj = result["object"].CopyNode(graph);
+
+                graph.Assert(new Triple(subject, predicate, obj));
+            }
+
+            CompressingTurtleWriter turtleWriter = new CompressingTurtleWriter();
+            string turtleString;
+            using (System.IO.StringWriter stringWriter = new System.IO.StringWriter())
+            {
+                turtleWriter.Save(graph, stringWriter);
+                turtleString = stringWriter.ToString();
+            }
+
+            var res = BH.Engine.Adapters.TTL.Convert.FromTTL(turtleString);
+
         }
 
         //[Test] 
