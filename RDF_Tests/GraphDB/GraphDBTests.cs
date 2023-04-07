34b6ae17edb5d0aa411ac597559f0ee1cb7b06ab
polnischfrosch <104036071+polnischfrosch@users.noreply.github.com>
2023-04-07 11:28:53 +0200

    Post Builds events added to GraphDB adapter and engine

--- RDF_Tests/GraphDB/GraphDBTests.cs
+++ RDF_Tests/GraphDB/GraphDBTests.cs
@@ -19,9 +19,27 @@ namespace RDF_Tests.GraphDB
     internal class GraphDBTests
     {
         [Test]
+        // Test requires GraphDB to be running
+        public void PushToGraphDB()
+        {
+            Room room = new Room();
+
+            string userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string TTLfilepath = Path.Combine(userDirectory, $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}_GraphDBPush.ttl");
+
+            CSharpGraph cSharpGraph = Compute.CSharpGraph(new List<object>() { room });
+            string TTLGraph = cSharpGraph.ToTTL(filepath: TTLfilepath);
+            string testRepositoryName = "TestRepository";
+
+            bool httpResponse = BH.Engine.Adapters.GraphDB.Compute.PostToRepo(TTLfilepath, repositoryName: testRepositoryName, run: true);
+
+            Assert.IsTrue(httpResponse);
+        }
+
+        [Test]
         public void PullFromRepoTest()
         {
-            var result = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(repositoryName:"myrepo123", run:true);
+            var result = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(repositoryName: "TestRepository", run:true);
             result.ShouldNotBeNull();
         }
 
@@ -29,11 +47,11 @@ namespace RDF_Tests.GraphDB
         public void PullFromRepoUserInput()
         {
             string userInput = "select ?subject ?predicate ?object WHERE {?subject ?predicate ?object.}";
-            var result = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(userInput,repositoryName: "myrepo123", run: true);
+            var result = BH.Engine.Adapters.GraphDB.Compute.PullFromRepo(userInput,repositoryName: "TestRepository", run: true);
             result.ShouldNotBeNull();
         }
 
-        [Test]
+        //[Test]
         public void PullFromRepoReadBack() 
         {
             string userInput = "PREFIX bh: <https://bhom.xyz/ontology/> SELECT ?predicate ?object WHERE {bh:BH.oM.Architecture.Elements.Room ?predicate ?object.}";
@@ -65,24 +83,6 @@ namespace RDF_Tests.GraphDB
 
             var res = BH.Engine.Adapters.TTL.Convert.FromTTL(turtleString);
 
-        }
-
-        //[Test] 
-        // Test requires GraphDB to be running
-        public void PushToGraphDB()
-        {
-            Room room = new Room();
-
-            string userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            string TTLfilepath = Path.Combine(userDirectory, $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}_GraphDBPush.ttl");
-
-            CSharpGraph cSharpGraph = Compute.CSharpGraph(new List<object>() { room });
-            string TTLGraph = cSharpGraph.ToTTL(filepath : TTLfilepath);
-            string testRepositoryName = "TestRepository";
-
-            bool httpResponse = BH.Engine.Adapters.GraphDB.Compute.PostToRepo(TTLfilepath, repositoryName: testRepositoryName, run: true);
-
-            Assert.IsTrue(httpResponse);
         }
     }
 }
