40f09a5ceccf622547c6fd662da9ff46e5062129
BHoMBot <BHoMBot@burohappold.com>
2024-01-02 14:21:15 +0000

    Uptick copyright headers to 2024

--- RDF_oM/Base64JsonSerialized.cs
+++ RDF_oM/Base64JsonSerialized.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -42,3 +42,4 @@ namespace BH.oM.Adapters.RDF
         // This class is not implementing IObject on purpose to distinguish it from all other BHoM Types.
     }
 }
+
